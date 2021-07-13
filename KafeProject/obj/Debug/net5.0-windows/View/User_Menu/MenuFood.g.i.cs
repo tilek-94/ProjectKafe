1507e7a497b0e4a804ff3e8f1da47645675fb19e
daniyarabdulhamidov@outlook.com <daniyarabdulhamidov@outlook.com>
2021-07-13 12:50:26 +0300

    DON

--- KafeProject/obj/Debug/net5.0-windows/View/User_Menu/MenuFood.g.i.cs
+++ KafeProject/obj/Debug/net5.0-windows/View/User_Menu/MenuFood.g.i.cs
@@ -190,7 +190,7 @@ namespace KafeProject.View.User_Menu {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/KafeProject;component/view/user_menu/menufood.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/KafeProject;V1.0.0.0;component/view/user_menu/menufood.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\View\User_Menu\MenuFood.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
