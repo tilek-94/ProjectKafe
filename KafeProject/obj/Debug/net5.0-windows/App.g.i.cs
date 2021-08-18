1d02e58393941131a3c739e27da24dc174625b7b
daniyarabdulhamidov@outlook.com <daniyarabdulhamidov@outlook.com>
2021-08-18 13:25:42 +0300

    as

--- KafeProject/obj/Debug/net5.0-windows/App.g.i.cs
+++ KafeProject/obj/Debug/net5.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace KafeProject {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/KafeProject;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/KafeProject;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
